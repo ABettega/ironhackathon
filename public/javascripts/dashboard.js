160e32aee954e4c8acdc6d241e37a98473fb9380
Gabriela Dias <gabrieladias.souza@hotmail.com>
2019-07-26 12:03:01 -0300

    Graphic on place, needs repairs

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,11 +1,8 @@
-const User = require("../models/User");
-
-
 const getDataInfo = () => {
   console.log();
-  User.find()
-    .then((response) => {
-      console.log(response)
+  axios.get('/admin/getLeads')
+  .then((response) => {
+      printTheChart(response.data.users)
     })
     .catch((error) => {
       console.log(error);
@@ -14,26 +11,32 @@ const getDataInfo = () => {
 
 getDataInfo()
 
-const printTheChart = ((stockData) => {
-  const stockLabels = [];
-  const stockPrice = [];
-  for (const key in stockData.bpi) {
-    stockLabels.push(key);
-    stockPrice.push(stockData.bpi[key]);
-  }
+const printTheChart = ((userData) => {
+  console.log(userData)
+  let myLabels = ['Access', 'Cupons']
+  let access = 0;
+  let cupons = 0;
+  let data = []
+  userData.forEach(user => {
+    access += 1
+    if (user.redeemed === true) {
+      cupons += 1;
+    }
+  })
+  data.push(access,cupons)
 
   const ctx = document.getElementById('myChart').getContext('2d');
   const chart = new Chart(ctx, {
-    type: 'line',
+    type: 'doughnut',
     data: {
-      labels: stockLabels,
+      labels: myLabels,
       datasets: [{
         label: 'Stock Chart',
         borderColor: 'rgb(255, 99, 132)',
         fill: false,
-        data: stockPrice,
+        data: data,
       }],
     },
   });
-  values(stockPrice);
+  values(data);
 });
