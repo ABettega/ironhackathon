b4cf3ebd81bfaeae86a230a51e5da23c937375d9
Aroldo Bettega <aroldo.bettega@gmail.com>
2019-07-26 11:17:56 -0300

    fixing QR

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router  = express.Router();
+const qr = require('qr-image')
 const User = require('../models/User');
 const Stores = require('../models/Stores');
 
@@ -21,6 +22,12 @@ router.get('/:coupon', (req, res, next) => {
   let { coupon } = req.params;
   User.findOneAndUpdate({generatedCoupon: {$eq: coupon }}, {$set: {redeemed: true}})
   .then(user => {
+    const url = `${process.env.CONFIRMATION_LINK}/${coupon}`; 
+    const code = qr.image(url, {type: 'svg'})
+    res.type('svg');
+    code.pipe(res)
+    console.log(res)
+    console.log(code)
     if (user.redeemed) {
       res.render('coupon', { user })
     } else {
